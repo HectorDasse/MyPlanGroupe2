de7896ecfcffb988b5f04fd27bd871c4ecc2e5dd
Hector <hector.dasse@orange.fr>
2020-11-04 15:43:29 +0100

    toto

--- myPlan/src/main/java/com/example/myPlan/Controller/MainController.java
+++ myPlan/src/main/java/com/example/myPlan/Controller/MainController.java
@@ -1,3 +1,8 @@
+package com.example.myPlan.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // This means that this class is a Controller
-@RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
+@RequestMapping(path="/") // This means URL's start with /demo (after Application path)
 public class MainController {
 
-    @GetMapping(path="/all")
+    @GetMapping(path="/toto")
     public @ResponseBody String toto() {
         // This returns a JSON or XML with the users
         return "toto";
