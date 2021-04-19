493482d63cb74288d4a65a4feb9d52871369a805
NawakChevelu <garfield-12@hotmail.fr>
2021-04-19 11:36:03 +0200

    [FEATURE] Ajout Device

---
+++ myPlan/src/main/java/com/example/myPlan/Entities/Device.java
@@ -0,0 +1,64 @@
+package com.example.myPlan.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Device {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String type; //interne/externe
+    private String number;
+
+    public Device(String name, String type, String number) {
+        this.name = name;
+        this.type = type;
+        this.number = number;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
