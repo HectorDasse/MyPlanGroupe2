2fc959480d3d1dd41cec8f6b25e51b56ad637c1d
sergeibux <sertux.dev@gmail.com>
2021-04-19 15:33:39 +0200

    [feature] CRUD : Collaborator

--- myPlan/src/main/java/com/example/myPlan/Controller/MainController.java
+++ myPlan/src/main/java/com/example/myPlan/Controller/MainController.java
@@ -1,15 +1,8 @@
 package com.example.myPlan.Controller;
 
 
-//import com.sun.xml.bind.v2.TODO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Controller // This means that this class is a Controller
 @RequestMapping(path="/") // This means URL's start with /demo (after Application path)
